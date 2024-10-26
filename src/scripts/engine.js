e463f648eae8160c94ed76addd1cb85d8c59951e
Ruan Manoel <ruuanmanoel@gmail.com>
2024-10-26 00:19:59 -0300

    ♻ refactor: Ajustando caminho dos recursos

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -18,10 +18,10 @@ const state={
     }
 }
 const playerSide={
-    player1: "player-field-card",
-    computer: "computer-field-card",
+    player1: "player-cards",
+    computer: "computer-cards",
 }
-const pathImages = ".src/assets/icons/"
+const pathImages = "./src/assets/icons/"
 const cardData =[
     {
         id:0,
@@ -55,7 +55,7 @@ async function getRandomCardId() {
 async function createCardImage(idCard, fieldSide) {
     const cardImage = document.createElement('img');
     cardImage.setAttribute('height', '100px');
-    cardImage.setAttribute('src', '.src/assets/icons/card-back.png');
+    cardImage.setAttribute('src', './src/assets/icons/card-back.png');
     cardImage.setAttribute('data-id',idCard);
     cardImage.classList.add('card');
 
